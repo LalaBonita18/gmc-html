e286cf7537755e3e74e5d558d8747207fd149b1c
LoujainaRab <loujaina18@gmail.com>
2025-01-06 11:42:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+/*console.log(typeof(true));
+console.log(typeof(9000));
+console.log(typeof("Übermensch"));
+console.log(typeof(anUndefinedVariable));
+console.log(typeof(null));
+var person1 = {
+	name : "foulan",
+	âge : 9000,
+	isStudent : true
+}
+console.log(typeof('Hello'))
+var a = 5;
+var b = a;
+var c = b;
+console.log(c);
+var a = 5;
+var b= 7;
+a+=b;
+console.log(a);
+var a = 3;
+a += 2;
+a -= 1;
+a **= 2;
+a /= 4;
+a %= 3;
+var result = a;
+console.log(result);
+var a = 0;
+a += 5;
+console.log(a);
+console.log(5 <= 5);
+console.log(5 < 5);
+console.log(5==5);
+console.log(5!=5);*/
+/*console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(""));
+console.log(Boolean(0));
+console.log(Boolean(false));
+var x;
+console.log(Boolean(x));
+var x = 5;
+var y = "5";
+console.log(x != y);
+console.log(null === true);*/
+/*var x = 1 // x est vrai
+var y = 0 // y est faux
+var z = null // z est faux
+console.log(x &&  y) // imprime 0
+console.log(z &&  y) // imprime null
+console.log(z || x) // imprime 1
+console.log(y || z) // s'imprime null
+var x = 0;
+var y = 10;
+var z = 5;
+console.log(z > x && z < y) // prints true
+console.log(z > x && z <= y) // prints true
+console.log(z > x && z != y) // prints true
+console.log(!!NaN) // imprime faux
+console.log(!!undefined) // imprime faux
+console.log(!!Infinity) // imprime vrai
+console.log(!!5) // imprime vrai*/
+console.log(!!NaN == !!undefined && 6 >= !!Infinity)
